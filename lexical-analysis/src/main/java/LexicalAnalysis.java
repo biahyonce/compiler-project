d12431e9aaa3f639e9539932f11b34f5ff64c9dd
nunesg <gustavonunes14.2011@gmail.com>
2020-11-30 22:15:48 -0300

    Fix AutomatonFileServiceImpl.java to parse transitions with blank spaces + add automaton resources

--- lexical-analysis/src/main/java/LexicalAnalysis.java
+++ lexical-analysis/src/main/java/LexicalAnalysis.java
@@ -3,7 +3,7 @@ import service.impl.AutomatonSimulationServiceImpl;
 public class LexicalAnalysis {
     public static void main(String[] args) {
         AutomatonSimulationServiceImpl simulator = new AutomatonSimulationServiceImpl();
-        String s = "+/*";
+        String s = "if a<b";
         simulator.simulate(s);
     }
 }
