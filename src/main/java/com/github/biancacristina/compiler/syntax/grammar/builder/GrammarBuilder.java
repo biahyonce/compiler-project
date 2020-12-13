b8e83ae5864d2ccee0d2a60614c1501dff691c37
BiancaCristina <biancacristina-silva@outlook.com>
2020-12-12 22:44:45 -0300

    Read first and follow

--- src/main/java/com/github/biancacristina/compiler/syntax/grammar/builder/GrammarBuilder.java
+++ src/main/java/com/github/biancacristina/compiler/syntax/grammar/builder/GrammarBuilder.java
@@ -5,6 +5,8 @@ import com.github.biancacristina.compiler.syntax.grammar.ItemInterface;
 import com.github.biancacristina.compiler.syntax.grammar.impl.Grammar;
 import com.github.biancacristina.compiler.syntax.grammar.impl.Production;
 import com.github.biancacristina.compiler.syntax.grammar.impl.Sentence;
+import com.github.biancacristina.compiler.syntax.grammar.request.FirstRequest;
+import com.github.biancacristina.compiler.syntax.grammar.request.FollowRequest;
 import com.github.biancacristina.compiler.syntax.grammar.request.ItemRequest;
 import com.github.biancacristina.compiler.syntax.grammar.request.RuleRequest;
 
@@ -16,9 +18,18 @@ public class GrammarBuilder {
 
     private GrammarBuilder(){}
 
-    public GrammarInterface build(ArrayList<RuleRequest> rules) {
+    public GrammarInterface build(ArrayList<RuleRequest> rules, ArrayList<FirstRequest> firstList, ArrayList<FollowRequest> followList) {
         GrammarInterface grammar = new Grammar();
         rules.forEach( rule -> buildRule(grammar, rule.getProduction(), rule.getSentences()) );
+        firstList.forEach( first -> {
+            Production production = (Production) grammar.get(first.getProduction());
+            System.out.println(production.getLabel());
+            production.addFirst(first.getFirst());
+        } );
+        followList.forEach( follow -> {
+            Production production = (Production) grammar.get(follow.getProduction());
+            production.addFollow(follow.getFollow());
+        } );
         return grammar;
     }
 
--- src/main/java/com/github/biancacristina/compiler/syntax/grammar/parser/impl/GrammarParserImpl.java
+++ src/main/java/com/github/biancacristina/compiler/syntax/grammar/parser/impl/GrammarParserImpl.java
@@ -2,6 +2,8 @@ package com.github.biancacristina.compiler.syntax.grammar.parser.impl;
 
 import com.github.biancacristina.compiler.syntax.grammar.exception.GrammarParserException;
 import com.github.biancacristina.compiler.syntax.grammar.parser.GrammarParser;
+import com.github.biancacristina.compiler.syntax.grammar.request.FirstRequest;
+import com.github.biancacristina.compiler.syntax.grammar.request.FollowRequest;
 import com.github.biancacristina.compiler.syntax.grammar.request.RuleRequest;
 
 import java.io.InputStream;
@@ -10,10 +12,12 @@ import java.util.Scanner;
 
 public class GrammarParserImpl implements GrammarParser {
     private static final String GRAMMAR_FILE = "/grammar.txt";
+    private static final String FIRST_FILE = "/first.txt";
+    private static final String FOLLOW_FILE = "/follow.txt";
     private static final String SPLIT_REGEX = "\\s+";
 
     @Override
-    public ArrayList<RuleRequest> read() {
+    public ArrayList<RuleRequest> readRules() {
         try {
             ArrayList<RuleRequest> rules = new ArrayList<>();
             InputStream inputStream = GrammarParserImpl.class.getResourceAsStream(GRAMMAR_FILE);
@@ -26,6 +30,40 @@ public class GrammarParserImpl implements GrammarParser {
             return rules;
         } catch (Exception e) {
             throw new GrammarParserException("Error on parsing grammar file", e);
+        }
+    }
+
+    @Override
+    public ArrayList<FirstRequest> readFirst() {
+        try {
+             ArrayList<FirstRequest> firstList = new ArrayList<>();
+             InputStream inputStream = GrammarParserImpl.class.getResourceAsStream(FIRST_FILE);
+             Scanner scanner = new Scanner(inputStream);
+             while (scanner.hasNextLine()) {
+                 String line = scanner.nextLine();
+                 String[] first = line.split(SPLIT_REGEX);
+                 firstList.add(new FirstRequest(first));
+             }
+             return firstList;
+        } catch (Exception e) {
+            throw new GrammarParserException("Error on parsing first file", e);
+        }
+    }
+
+    @Override
+    public ArrayList<FollowRequest> readFollow() {
+        try {
+            ArrayList<FollowRequest> followList = new ArrayList<>();
+            InputStream inputStream = GrammarParserImpl.class.getResourceAsStream(FOLLOW_FILE);
+            Scanner scanner = new Scanner(inputStream);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] follow = line.split(SPLIT_REGEX);
+                followList.add(new FollowRequest(follow));
+            }
+            return followList;
+        } catch (Exception e) {
+            throw new GrammarParserException("Error on parsing follow file", e);
         }
     }
 }
